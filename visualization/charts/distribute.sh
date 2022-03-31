90e4a66abb53cec5b03829b7d4214aa027d316b7
Ioachim Lihor <ioachim.lihor@gmail.com>
2022-03-31 20:27:20 +0300

    [LC-8] more html and png visualization on fake and valid date
    Signed-off-by: Ioachim Lihor <ioachim.lihor@gmail.com>

---
+++ visualization/charts/distribute.sh
@@ -0,0 +1,25 @@
+mkdir html
+mkdir html/basics
+mkdir html/kw
+mkdir html/ner
+mkdir html/pos
+mkdir html/tfidf
+
+mv KeyWord*.html html/kw
+mv NER*.html html/ner
+mv POS*.html html/pos
+mv TFIDF*.html html/tfidf
+mv *.html html/basics
+
+mkdir png
+mkdir png/basics
+mkdir png/kw
+mkdir png/ner
+mkdir png/pos
+mkdir png/tfidf
+
+mv KeyWord*.png png/kw
+mv NER*.png png/ner
+mv POS*.png png/pos
+mv TFIDF*.png png/tfidf
+mv *.png png/basics
