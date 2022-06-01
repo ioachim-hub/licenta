4c45d30f5ab961a933ff8a1674bd8a80baf56ca4
Ioachim Lihor <ioachim.lihor@gmail.com>
2022-06-01 21:33:10 +0300

    [WORK] add favicon.ico and js_check
    Signed-off-by: Ioachim Lihor <ioachim.lihor@gmail.com>

---
+++ cluster/fakepred/predicter/assets/js/js_check.js
@@ -0,0 +1,34 @@
+var check = function () {
+  if (
+    document.getElementById("link").value.length != 0 &&
+    (document.getElementById("title").value.length == 0 ||
+      document.getElementById("content").value.length == 0)
+  ) {
+    document.getElementById("title").disabled = true;
+    document.getElementById("content").disabled = true;
+    document.getElementById("submit").disabled = false;
+  } else if (
+    document.getElementById("link").value.length == 0 &&
+    (document.getElementById("title").value.length != 0 ||
+      document.getElementById("content").value.length != 0)
+  ) {
+    document.getElementById("link").disabled = true;
+    document.getElementById("submit").disabled = false;
+  }
+  if (
+    document.getElementById("link").value.length == 0 &&
+    document.getElementById("title").disabled == true &&
+    document.getElementById("content").disabled == true
+  ) {
+    document.getElementById("title").disabled = false;
+    document.getElementById("content").disabled = false;
+    document.getElementById("submit").disabled = true;
+  } else if (
+    document.getElementById("title").value.length == 0 &&
+    document.getElementById("content").value.length == 0 &&
+    document.getElementById("link").disabled == true
+  ) {
+    document.getElementById("link").disabled = false;
+    document.getElementById("submit").disabled = true;
+  }
+};
